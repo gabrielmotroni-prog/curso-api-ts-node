f0644571123364e2590487115c42036887c4226a
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2022-07-23 17:22:18 -0300

    api base

--- dist/repository/newsRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const newsSchema_1 = require("../models/newsSchema");
-//mapear a collection a ser criada no banco com a model
-//Serve para mapear nossa model com a collection no db
-exports.default = mongoose.model("news", newsSchema_1.default);
--- repository/newsRepository.ts
+++
@@ -1,6 +0,0 @@
-import * as mongoose from 'mongoose'
-import NewsSchema from '../models/newsSchema'
-
-//mapear a collection a ser criada no banco com a model
-//Serve para mapear nossa model com a collection no db
-export default mongoose.model("news", NewsSchema)
