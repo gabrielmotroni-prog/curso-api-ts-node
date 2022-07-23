75108d8904f426ecf6e5792d5c170e45d2f5cbca
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2022-07-23 17:28:09 -0300

    api base com mongoDB e docker

--- dist/infra/db.js
+++ dist/infra/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //clase de conexao com db
 class Database {
     constructor() {
-        this.DB_URL = "mongodb://localhost:27017/db_portal"; // vamos apontar para db do docker
+        this.DB_URL = "mongodb://link-db:27017/db_portal"; // vamos apontar para db do docker
     }
     createConnection() {
         mongoose.connect(this.DB_URL);
--- infra/db.ts
+++ infra/db.ts
@@ -2,7 +2,7 @@ import * as mongoose from "mongoose";
 
 //clase de conexao com db
 class Database {
-    private DB_URL = "mongodb://localhost:27017/db_portal" // vamos apontar para db do docker
+    private DB_URL = "mongodb://link-db:27017/db_portal" // vamos apontar para db do docker
 
     createConnection(){
         mongoose.connect(this.DB_URL)
