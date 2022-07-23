84f462b036ac8cf1634625869e1aa40885b917e1
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2022-07-23 17:41:30 -0300

    atualizando infra/db

--- infra/db.ts
+++ infra/db.ts
@@ -2,7 +2,7 @@ import * as mongoose from "mongoose";
 
 //clase de conexao com db
 class Database {
-    private DB_URL = "mongodb://link-db:27017/db_portal" // vamos apontar para db do docker
+    private DB_URL = "mongodb://link-db/db_portal" // link-db : link presente no docker-compose. Sobe container nas portas especificadas
 
     createConnection(){
         mongoose.connect(this.DB_URL)
