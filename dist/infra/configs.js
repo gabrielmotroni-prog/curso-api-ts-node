bcdb0235e4593155bb18abf3072fab7ab1c8925d
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2022-07-26 01:41:29 -0300

    atualizacao ignore

--- dist/infra/configs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Configs {
-    constructor() {
-        this.secret = "batman batman batman"; //chave para jwt
-    }
-}
-exports.default = new Configs;
