1ab2fea82c1e01d0daa9a16f112fc4f3f8684294
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2022-09-11 15:15:39 -0300

    aulla 126

---
+++ graphql/types/newsType.ts
@@ -0,0 +1,16 @@
+export default
+`
+scalar Date 
+
+type News{
+    _id: String,
+    hat: String,
+    title: String,
+    author: String,
+    img: String,
+    publishDate: Date,
+    link: String,
+    tag: String,
+    active: Boolean
+}
+`
