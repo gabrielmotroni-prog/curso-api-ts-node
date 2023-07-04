91d7b1d7bf7b0fdd8a6a6f2d10963fa4ff252f44
C1325300 Gabriel Dos Santos Motroni <gabrielmotroni@ibm.com>
2023-07-04 20:56:04 -0300

    correcao parcial

--- graphql/schemas.ts
+++ graphql/schemas.ts
@@ -1,13 +1,11 @@
 /*
  responsável por passar nosso type mapeado com nosssas model para o graphQL
 */
-import { buildSchema, GraphQLSchema} from "graphQL"
-import newsType from "./types/newsType"
+import { buildSchema, GraphQLSchema } from "graphQL";
+import newsType from "./types/newsType";
 
 //coloquei : any no schema pq estava dando algum erro ao cham-lo no startUp.ts
 // passamos todos nossos schemas mapeados para `schema`
-const schema: GraphQLSchema =  buildSchema(
-    newsType 
-)
+const schema = buildSchema(newsType);
 
-export default schema
+export default schema;
--- infra/db.ts
+++ infra/db.ts
@@ -2,16 +2,22 @@ import * as mongoose from "mongoose";
 
 //clase de conexao com db
 class Database {
-    private DB_URL = "mongodb://localhost:27017/db_portal" 
-    //private DB_URL = "mongodb://link-db/db_portal" 
-
-    async createConnection(){
-        try{
-            await mongoose.connect(this.DB_URL)
-        }catch(error){
-            console.error(`# Erro ao conectar com banco de dados mongoDB: \n ${error}`)
-        }
+  private DB_URL = "mongodb://localhost:27017/db_portal";
+  //private DB_URL = "mongodb://link-db/db_portal"
+  useUnifiedTopology: true;
+  async createConnection() {
+    try {
+      await mongoose.connect(this.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      });
+    } catch (error) {
+      console.error(
+        `# Erro ao conectar com banco de dados mongoDB: \n ${error}`
+      );
     }
+  }
 }
 
 export default Database;
